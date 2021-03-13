5ea4cd84bc41c354f1f6a4f0374a3b1d911ac1ab
saubun <dameon0430@gmail.com>
2021-03-13 18:56:18 -0500

    feat: added css styles and made navbar
    BREAKING CHANGE: yes first commit

---
+++ src/components/Layout.tsx
@@ -0,0 +1,20 @@
+import React, { useEffect } from 'react';
+import Navbar from './Navbar';
+
+export default function Layout({ children }: any) {
+  useEffect(() => {
+    document.addEventListener(`scroll`, () => {
+      document.documentElement.dataset.scroll = window.scrollY.toString();
+    });
+  }, []);
+
+  return (
+    <div className="main">
+      <Navbar />
+      {/* Meta */}
+      <div className="container">
+        <main>{children}</main>
+      </div>
+    </div>
+  );
+}
